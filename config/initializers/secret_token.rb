bda5ca4d1453077db559b648b4469306548f7b74
AK <dnamax@gmail.com>
2011-01-18 19:08:58 -0800

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorld::Application.config.secret_token = 'b42609167983ac8e1ab36a3bee53f2cbf1e735d6e8adbb53cdb4f8d2a6c0aa3d0ee66255f0db8a3b4793a19a181d85cc488c8ca1ce0851aec548e2f40946e9a2'
